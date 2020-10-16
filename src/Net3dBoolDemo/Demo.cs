e360f0faffc7d0aba6394bba8bed4a323487d0be
Ibrahim Saad <ibrahim.saad@diroots.com>
2020-10-16 13:51:56 +0200

    library ported as a dotnet strandard lib. remove the dependency on the opentoolkit.

--- src/Net3dBoolDemo/Demo.cs
+++ src/Net3dBoolDemo/Demo.cs
@@ -1,85 +1,82 @@
-﻿using System;
-using System.Drawing;
-using OpenToolkit;
-using OpenToolkit.Graphics;
-using OpenToolkit.Graphics.OpenGL;
-using OpenToolkit.Input;
+﻿using OpenToolkit.Graphics.OpenGL;
 using OpenToolkit.Mathematics;
+using System;
+using System.Drawing;
 
 namespace Net3dBoolDemo
 {
 
-    public class Demo : Window
+  public class Demo : Window
+  {
+
+    public Net3dBool.Solid Mesh;
+
+    public override void CreateMesh()
     {
+      var start = DateTime.UtcNow;
+      Console.Write("Generate mesh...");
 
-        public Net3dBool.Solid Mesh;
+      var box = new Net3dBool.Solid(Net3dBool.DefaultCoordinates.DEFAULT_BOX_VERTICES, Net3dBool.DefaultCoordinates.DEFAULT_BOX_COORDINATES);
+      var sphere = new Net3dBool.Solid(Net3dBool.DefaultCoordinates.DEFAULT_SPHERE_VERTICES, Net3dBool.DefaultCoordinates.DEFAULT_SPHERE_COORDINATES);
+      sphere.Scale(0.68, 0.68, 0.68);
 
-        public override void CreateMesh()
-        {
-            var start = DateTime.UtcNow;
-            Console.Write("Generate mesh...");
+      var cylinder1 = new Net3dBool.Solid(Net3dBool.DefaultCoordinates.DEFAULT_CYLINDER_VERTICES, Net3dBool.DefaultCoordinates.DEFAULT_CYLINDER_COORDINATES);
+      cylinder1.Scale(0.38, 1, 0.38);
 
-            var box = new Net3dBool.Solid(Net3dBool.DefaultCoordinates.DEFAULT_BOX_VERTICES, Net3dBool.DefaultCoordinates.DEFAULT_BOX_COORDINATES);
-            var sphere = new Net3dBool.Solid(Net3dBool.DefaultCoordinates.DEFAULT_SPHERE_VERTICES, Net3dBool.DefaultCoordinates.DEFAULT_SPHERE_COORDINATES);
-            sphere.Scale(0.68, 0.68, 0.68);
+      var cylinder2 = new Net3dBool.Solid(Net3dBool.DefaultCoordinates.DEFAULT_CYLINDER_VERTICES, Net3dBool.DefaultCoordinates.DEFAULT_CYLINDER_COORDINATES);
+      cylinder2.Scale(0.38, 1, 0.38);
+      cylinder2.Rotate(Math.PI / 2, 0);
 
-            var cylinder1 = new Net3dBool.Solid(Net3dBool.DefaultCoordinates.DEFAULT_CYLINDER_VERTICES, Net3dBool.DefaultCoordinates.DEFAULT_CYLINDER_COORDINATES);
-            cylinder1.Scale(0.38, 1, 0.38);
+      var cylinder3 = new Net3dBool.Solid(Net3dBool.DefaultCoordinates.DEFAULT_CYLINDER_VERTICES, Net3dBool.DefaultCoordinates.DEFAULT_CYLINDER_COORDINATES);
+      cylinder3.Scale(0.38, 1, 0.38);
+      cylinder3.Rotate(Math.PI / 2, 0);
+      cylinder3.Rotate(0, Math.PI / 2);
 
-            var cylinder2 = new Net3dBool.Solid(Net3dBool.DefaultCoordinates.DEFAULT_CYLINDER_VERTICES, Net3dBool.DefaultCoordinates.DEFAULT_CYLINDER_COORDINATES);
-            cylinder2.Scale(0.38, 1, 0.38);
-            cylinder2.Rotate(Math.PI / 2, 0);
+      var modeller = new Net3dBool.BooleanModeller(box, sphere);
+      var tmp = modeller.GetIntersection();
 
-            var cylinder3 = new Net3dBool.Solid(Net3dBool.DefaultCoordinates.DEFAULT_CYLINDER_VERTICES, Net3dBool.DefaultCoordinates.DEFAULT_CYLINDER_COORDINATES);
-            cylinder3.Scale(0.38, 1, 0.38);
-            cylinder3.Rotate(Math.PI / 2, 0);
-            cylinder3.Rotate(0, Math.PI / 2);
+      modeller = new Net3dBool.BooleanModeller(tmp, cylinder1);
+      tmp = modeller.GetDifference();
 
-            var modeller = new Net3dBool.BooleanModeller(box, sphere);
-            var tmp = modeller.GetIntersection();
+      modeller = new Net3dBool.BooleanModeller(tmp, cylinder2);
+      tmp = modeller.GetDifference();
 
-            modeller = new Net3dBool.BooleanModeller(tmp, cylinder1);
-            tmp = modeller.GetDifference();
+      modeller = new Net3dBool.BooleanModeller(tmp, cylinder3);
+      tmp = modeller.GetDifference();
 
-            modeller = new Net3dBool.BooleanModeller(tmp, cylinder2);
-            tmp = modeller.GetDifference();
+      Mesh = tmp;
 
-            modeller = new Net3dBool.BooleanModeller(tmp, cylinder3);
-            tmp = modeller.GetDifference();
-
-            Mesh = tmp;
-
-            var elapsed = DateTime.UtcNow - start;
-            Console.WriteLine("done.");
-            Console.WriteLine("Consumed time: {0}", elapsed);
-        }
-
-        public override void RenderMesh()
-        {
-            GL.Begin(PrimitiveType.Triangles);
-
-            var verts = Mesh.GetVertices();
-            int[] ind = Mesh.GetIndices();
-
-            for (var i = 0; i < ind.Length; i = i + 3)
-            {
-                GL.Normal3(new Vector3(1, 1, 1));
-                var p = verts[ind[i]];
-                GL.Color3(Color.Red);
-                GL.Vertex3(new Vector3d(p.X, p.Y, p.Z));
-
-                p = verts[ind[i + 1]];
-                GL.Color3(Color.Blue);
-                GL.Vertex3(new Vector3d(p.X, p.Y, p.Z));
-
-                p = verts[ind[i + 2]];
-                GL.Color3(Color.Green);
-                GL.Vertex3(new Vector3d(p.X, p.Y, p.Z));
-            }
-
-            GL.End();
-        }
-
+      var elapsed = DateTime.UtcNow - start;
+      Console.WriteLine("done.");
+      Console.WriteLine("Consumed time: {0}", elapsed);
     }
+
+    public override void RenderMesh()
+    {
+      GL.Begin(PrimitiveType.Triangles);
+
+      var verts = Mesh.GetVertices();
+      int[] ind = Mesh.GetIndices();
+
+      for (var i = 0; i < ind.Length; i = i + 3)
+      {
+        GL.Normal3(new Vector3(1, 1, 1));
+        var p = verts[ind[i]];
+        GL.Color3(Color.Red);
+        GL.Vertex3(new Vector3d(p.X, p.Y, p.Z));
+
+        p = verts[ind[i + 1]];
+        GL.Color3(Color.Blue);
+        GL.Vertex3(new Vector3d(p.X, p.Y, p.Z));
+
+        p = verts[ind[i + 2]];
+        GL.Color3(Color.Green);
+        GL.Vertex3(new Vector3d(p.X, p.Y, p.Z));
+      }
+
+      GL.End();
+    }
+
+  }
 }
 
--- src/Net3dBoolDemo/Program.cs
+++ src/Net3dBoolDemo/Program.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using OpenToolkit;
-
-namespace Net3dBoolDemo
+﻿namespace Net3dBoolDemo
 {
-    class MainClass
+  class MainClass
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            var demo = new Demo();
+      var demo = new Demo();
 
-            // Run the game at 60 updates per second
-            demo.Run();
-        }
-
+      // Run the game at 60 updates per second
+      demo.Run();
     }
+
+  }
 }
